9735f000deb86af2b27b413e3cbd45846e5fbdcb
dhereau <dhereau993@gmail.com>
2023-11-18 14:47:08 +0530

    second commit

---
+++ src/main/java/Pakage/anuradha.java
@@ -0,0 +1,10 @@
+package Pakage;
+
+public class anuradha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&");
+	}
+
+}
