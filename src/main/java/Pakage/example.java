72fa6ca62701277ab9efa662707a5e2c756a2524
dhereau <dhereau993@gmail.com>
2023-11-18 14:57:44 +0530

    third commit

---
+++ src/main/java/Pakage/example.java
@@ -0,0 +1,15 @@
+package Pakage;
+
+public class example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&");System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&");System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&");System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&");System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&");System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&");System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&");
+	}
+
+}
